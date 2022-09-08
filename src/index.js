21b668a4744d8ef3b85ed9756a76ef5a4c72fc8c
Yuliya Zozulya <djulia@ukr.net>
2022-09-08 14:51:14 +0300

    Виправляє функцію прослуховувння кнопки

--- src/index.js
+++ src/index.js
@@ -3,31 +3,27 @@ import axios from "axios"
 const input = document.querySelector('#search-form')
 const inputBtn = document.querySelector('[type=submit]')
 
-// inputBtn.addEventListener('submit', onRanderPicturesBtn)
+inputBtn.addEventListener('submit', onRanderPicturesBtn)
 input.addEventListener('keyup', onKeyWordInput)
 
 function onKeyWordInput (evt) {
 const keyWordInput = evt.currentTarget.elements[0].value
 console.log(keyWordInput)
-
-async function getUser() {
-    try {
-      const response = await axios.get(`https://pixabay.com/api/?key=29781267-a8728f24297a8bee7a02bc916&q=${keyWordInput}&image_type=photo`);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-
 }
 
-async function getUser() {
+function onRanderPicturesBtn (evt) {
+console.log(evt)
+}
+
+async function getRequest() {
     try {
-      const response = await axios.get('https://pixabay.com/api/?key=29781267-a8728f24297a8bee7a02bc916&q=yellow+flowers&image_type=photo');
+      const response = await axios.get(`https://pixabay.com/api/?key=29781267-a8728f24297a8bee7a02bc916&q=${keyWordInput}&image_type=photo&orientation=horizontal&safesearch=true`);
+      
       console.log(response);
     } catch (error) {
       console.error(error);
     }
   }
+
+
 
